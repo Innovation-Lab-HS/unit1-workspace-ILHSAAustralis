f5886bef7dfcd891dea7bad6f6b14af4cf77e1a2
ILHSAAustralis <2014993@apps.nsd.org>
2024-09-13 18:20:24 +0000

    blah

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+  // all code in Java sits inside a class
+  
+  // every java program requires a public main method
+  // for the next while will live inside main
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println();
+    System.out.println("Goodbye World");
+  }
+
+  
+}
